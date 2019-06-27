f3cf100619399c6681cc29869a91588321e40435
韦其政 <334064275@163.com>
2019-06-27 10:42:10 +0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Introduction
+
+This is a simple demo for weex.
+
+## Quickstart
+
+``` bash
+$ npm install -g weex-toolkit
+$ git clone https://github.com/dearwayne/weex-simple-demo.git
+$ cd weex-simple-demo
+$ npm install
+$ npm start
+```
---
+++ src/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="wrapper">
+        <text>this is a weex test project!</text>
+        <text>adsfasdfasd</text>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+    .wrapper {
+        justify-content: center;
+        align-items: center;
+    }
+</style>
